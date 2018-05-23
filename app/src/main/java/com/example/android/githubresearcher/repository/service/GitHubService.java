ee8f537411eb251cb71d7082c01c2078514d5d43
João Paulo Lins <jplo@cin.ufpe.br>
2018-05-22 22:55:59 -0300

    Tela de login e mudança do GitHubService e Pojo para consumir API privada

--- app/src/main/java/com/example/android/githubresearcher/repository/service/GitHubService.java
+++ app/src/main/java/com/example/android/githubresearcher/repository/service/GitHubService.java
@@ -1,22 +1,14 @@
 package com.example.android.githubresearcher.repository.service;
 
-import com.example.android.githubresearcher.repository.service.pojo.RepositoryPojo;
 import com.example.android.githubresearcher.repository.service.pojo.UserPojo;
-
-import java.util.List;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface GitHubService {
 
-    @GET("users/{name}")
-    Observable<UserPojo> getUser(@Path("name") String name);
-
-    @GET("users/{name}/repos")
-    Observable<List<RepositoryPojo>> getUserRepository(@Path("{name}") String name,
-                                                       @Query("page") int page,
-                                                       @Query("per_page") int perPage);
+    @GET("autentica/{username}/{password}")
+    Observable<UserPojo> getUser(@Path("username") String username,
+                                 @Path("password") String password);
 }
