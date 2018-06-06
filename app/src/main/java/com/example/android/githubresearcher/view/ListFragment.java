e6b0b8aa9f89967787deabc3926467f2563d069a
João Paulo Lins <jplo@cin.ufpe.br>
2018-06-06 07:01:00 -0300

    Card de adicionar nova lista em ListFragment

--- app/src/main/java/com/example/android/githubresearcher/view/ListFragment.java
+++ app/src/main/java/com/example/android/githubresearcher/view/ListFragment.java
@@ -1,6 +1,8 @@
 package com.example.android.githubresearcher.view;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +10,25 @@ import android.view.ViewGroup;
 
 import com.example.android.githubresearcher.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ListFragment extends Fragment{
+
+    @BindView(R.id.list_wrapper)
+    TextInputLayout listWrapper;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listWrapper.setHint("New list name");
     }
 }
